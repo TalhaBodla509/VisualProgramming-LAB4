ef774ddd1143ee3fecc80406a31957fcd8b620a0
TalhaBodla509 <zaidbodla786@gmail.com>
2024-10-23 13:35:45 +0500

    Add files via upload

---
+++ VP LAB 4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Lab5
+{
+
+    static void Main(string[] args)
+    {
+        /* double a = 98, b = 0;
+         double result = 0;
+         try
+         {
+             result = SafeDivision(a, b);
+             Console.WriteLine($"{a} divided by {b}={result}");
+         }
+         catch (DivideByZeroException)
+         {
+             Console.WriteLine("An Attempt Divided By zero.");
+         }
+        */
+        TestCatch2();
+    }
+    static void TestCatch2()
+    {
+        System.IO.StreamWriter sw=null ;
+        try
+        {
+            sw = new System.IO.StreamWriter(@"C:\test\test.txt");
+            sw.WriteLine("Hello Whats up dude");
+        }
+        catch (System.IO.FileNotFoundException ex)
+        {
+            System.Console.WriteLine(ex.ToString());
+
+        }
+        catch(System.IO.IOException ex)
+        { System.Console.WriteLine(ex.ToString());}
+        finally
+        {
+            sw.Close();
+        }
+    }
+    /*static double SafeDivision(double k, double l)
+    {
+        if (l == 0)
+            throw new System.DivideByZeroException();
+        return k / l;
+    }*/
+}
